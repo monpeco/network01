de72dab3dba0d9d2aa50c59099a4d69c90a8917d
Alejandro Montilla <monpeco@gmail.com>
2021-05-15 18:16:33 +0200

    Use state in App for testBenchSelected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, { useState } from 'react'
+
 import Title from './components/title'
+import Configuration from './components/configuration'
 
 function App() {
+  const [ testBenchSelected, setTestBenchSelected ] = useState([])
+  console.log(testBenchSelected)
   return (
     <div className="App">
       <Title />
+      <Configuration testBenchSelected={testBenchSelected} setTestBenchSelected={setTestBenchSelected} />
     </div>
   );
 }
