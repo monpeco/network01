85ea14a835476f0086b4ce98a49a5be009c1c7fb
Alejandro Montilla <monpeco@gmail.com>
2021-05-16 16:01:03 +0200

    Add DBNetwork component and network data base

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 
 import Title from './components/title'
 import Configuration from './components/configuration'
+import DBNetwork from './components/DBNetwork'
+import data from './data/networksDataBase.json'
 
 function App() {
   const [ testBenchSelected, setTestBenchSelected ] = useState([])
   const [ measurementFile, setMeasurementFile ] = useState('')
+  console.log(data)
   return (
     <div className="App">
       <Title />
       <Configuration measurementFile={measurementFile} setMeasurementFile={setMeasurementFile} testBenchSelected={testBenchSelected} setTestBenchSelected={setTestBenchSelected} />
+      <DBNetwork network={data.networks[0]} />
+      <DBNetwork network={data.networks[1]} />
     </div>
   );
 }
