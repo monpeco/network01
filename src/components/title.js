8a302061b79da2179da3165bed4c3ebcef091a52
Alejandro Montilla <monpeco@gmail.com>
2021-05-17 08:14:05 +0200

    Add Paper to Title component

--- src/components/title.js
+++ src/components/title.js
@@ -1,16 +1,18 @@
 import React, { PureComponent, Component } from 'react';
-import { Container, Typography } from '@material-ui/core'
+import { Container, Typography, Paper } from '@material-ui/core'
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 class Title extends Component {
     render() { 
         return (
             <Container>
-                <Typography variant="h4">CARM network performance test GUI - 0.2a</Typography>
-                <Typography variant="body1" color="textSecondary">
-                    <EmojiObjectsIcon />
-                    Ask MPSM team
-                </Typography>
+                <Paper elevation={3}>
+                    <Typography variant="h4">CARM network performance test GUI - 0.2a</Typography>
+                    <Typography variant="body1" color="textSecondary">
+                        <EmojiObjectsIcon />
+                        Ask MPSM team
+                    </Typography>
+                </Paper>
             </Container>
         );
     }
